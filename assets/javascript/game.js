a0524fa06d2b0d97e92aedfb0d0e593ccc4325d6
Matthew Amis <mfamis@gmail.com>
2018-06-21 18:29:48 -0700

    Adding full document listener to try and intercept mobile keyboard input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -209,8 +209,7 @@ function game_input(event)
 
 // Grab keystrokes and apply them to the game
 document.onkeyup = game_input;
-
-document.getElementById("hidden-input").addEventListener("keydown", game_input); 
+document.addEventListener("keydown input", game_input);
 
 // Initialize the game and display
 game_state.reset();
